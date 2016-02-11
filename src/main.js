3d5f1d999112583f01b20e21d49b9985bd97ccf3
Stephen Sherratt <steve@stevebob.net>
2016-02-12 09:56:11 +1100

    Added test of tiles

--- src/main.js
+++ src/main.js
@@ -1,30 +1,44 @@
 import './populate_namespaces.js';
 
-import {Components} from './components.js';
-import {InvalidatingComponentTable} from './invalidating_component_table.js';
+import {TileStore} from './tile_store.js';
+import {loadImage} from './image_loader.js';
+
+import {drawer} from './singleton_drawer.js';
 
 export async function main() {
-    var table = new InvalidatingComponentTable();
 
-    var a = new Components.Position().init(1, 2);
-    var b = new Components.Position().init(3, 4);
+    var ts = new TileStore(20, 20);
 
-    a.blah = 123;
+    ts.setFont('ps2p', 16, false, false);
+    ts.yOffset = 2;
 
-    console.debug(table);
-    console.debug(table.get(Components.Position));
+    var a = ts.allocateCharacterTile('A');
 
-    table.add(a);
+    ts.fontBold = true;
+    ts.updateFont();
 
-    var x = table.get(Components.Position);
-    console.debug(x, x == a);
+    var b = ts.allocateCharacterTile('A', 'red', 'rgba(0,0,0,0)');
+    var c = ts.allocateCharacterTile('@', 'white', 'black');
+    var d = ts.allocateImage(await loadImage('images/wall-front.png'));
+    var e = ts.allocateCharacterTile('b', 'black', 'white');
+    var f = ts.allocateCharacterTile('.', 'white', 'black');
+    var g = ts.allocateImage(await loadImage('images/wall-top.png'));
 
-    table.remove(Components.Position);
+    drawer.drawTile(a, 10, 20);
+    drawer.drawTile(b, 100, 50);
 
-    console.debug(table.get(Components.Position));
+    drawer.drawTile(g, 0, 0);
+    drawer.drawTile(g, 60, 0);
+    drawer.drawTile(g, 0, 20);
+    drawer.drawTile(g, 60, 20);
+    drawer.drawTile(d, 0, 40);
+    drawer.drawTile(d, 60, 40);
 
-    table.add(b);
-    var y = table.get(Components.Position);
-    console.debug(y, x == y);
+    drawer.drawTile(d, 20, 0);
+    drawer.drawTile(d, 40, 0);
 
+    drawer.drawTile(f, 20, 40);
+    drawer.drawTile(f, 40, 40);
+    drawer.drawTile(c, 20, 20);
+    drawer.drawTile(f, 40, 20);
 }
