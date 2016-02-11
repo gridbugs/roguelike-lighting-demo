df5a1e3816cb1def41e0511af6ea06420b79a923
Stephen Sherratt <steve@stevebob.net>
2016-02-12 01:06:07 +1100

    Moved cloning into individual components

--- src/component.js
+++ src/component.js
@@ -2,17 +2,12 @@ import {Typed} from './type.js';
 export class Component extends Typed {
     constructor(...args) {
         super();
+        this._ = this.name;
         this.valid = true;
     }
 
     /* Turns dest into a copy of this */
     copyTo(dest) {
         dest.valid = this.valid;
-    }
-
-    clone() {
-        let ret = new this.constructor();
-        this.copyTo(ret);
-        return ret;
     }
 }
--- src/core_component.js
+++ src/core_component.js
@@ -2,17 +2,17 @@ import {Component} from './component.js';
 import {Vec2} from './vec2.js';
 
 export class Position extends Component {
-    constructor() {
+    constructor(x, y) {
         super();
-        this.vector = new Vec2(0, 0);
-    }
 
-    init(x, y) {
+        this.vector = new Vec2(0, 0);
+
         if (typeof x === 'number') {
             this.vector.set(x, y);
         } else {
             x.copyTo(this.vector);
         }
+
         return this;
     }
 
@@ -20,16 +20,41 @@ export class Position extends Component {
         super.copyTo(dest);
         this.vector.copyTo(dest.vector);
     }
+
+    clone() {
+        return new Position(this.vector);
+    }
+
+    get x() {
+        return this.vector.x;
+    }
+
+    get y() {
+        return this.vector.y;
+    }
+
+    set x(value) {
+        this.vector.x = value;
+    }
+
+    set y(value) {
+        this.vector.y = value;
+    }
 }
 
 export class Tile extends Component {
-
+    constructor(character) {
+        super();
+        this.character = character;
+    }
 }
 
 export class Solid extends Component {
-
 }
 
 export class Opacity extends Component {
-
+    constructor(value) {
+        super();
+        this.value = value;
+    }
 }
