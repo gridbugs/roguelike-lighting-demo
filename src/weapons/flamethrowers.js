45c353ddbd68d4dea3b8a1c5a254088ee143bcab
Stephen Sherratt <steve@stevebob.net>
2016-03-05 21:48:23 +1100

    Fixed out of bounds reference when flamer is used close to the edge of the map

--- src/weapons/flamethrowers.js
+++ src/weapons/flamethrowers.js
@@ -8,11 +8,10 @@ export class Flamethrower extends Weapon {
         super();
         this.maxAmmo = 40;
         this.ammo = 0;
-        this.flameSpread = 1 / 4; // 45 degree cone
+        this.flameSpread = 1 / 4;
         this.range = 10;
         this.lowerRange = this.range - 1;
         this.upperRange = this.range + 1;
-        this.spreadWidth = this.flameSpread * this.range;
         this.burstLength = 4;
     }
 
@@ -20,12 +19,20 @@ export class Flamethrower extends Weapon {
         Weapon.SpreadStack.clear();
         let startCell;
         for (let coord of line.infiniteAbsoluteCoords()) {
+            if (!Weapon.SpreadGrid.isValid(coord)) {
+                break;
+            }
+
+            startCell = Weapon.SpreadGrid.get(coord);
+
             if (coord.getDistance(line.startCoord) >= this.range) {
-                startCell = Weapon.SpreadGrid.get(coord);
                 break;
             }
         }
-        for (let cell of startCell.floodFill(Directions, this.spreadWidth)) {
+
+        let spreadWidth = this.flameSpread * startCell.coord.getDistance(line.startCoord);
+
+        for (let cell of startCell.floodFill(Directions, spreadWidth)) {
             let distance = cell.coord.getDistance(line.startCoord);
             if (distance >= this.lowerRange && distance <= this.upperRange) {
                 Weapon.SpreadStack.push(cell);
