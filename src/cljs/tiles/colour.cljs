98f34cb35fdaa683817dba111c8072e68a5ab0a4
Stephen Sherratt <steve@stevebob.net>
2016-04-16 20:25:46 +1000

    Removed erroneous spaces from rgba colours in lisp image library

--- src/cljs/tiles/colour.cljs
+++ src/cljs/tiles/colour.cljs
@@ -1,7 +1,7 @@
 (ns tiles.colour)
 
-(defn rgba [r g b a] (str "rgba(" r ", " g ", " b ", " a ")"))
-(defn rgb [r g b] (str "rgb(" r ", " g ", " b ")"))
+(defn rgba [r g b a] (str "rgba(" r "," g "," b "," a ")"))
+(defn rgb [r g b] (str "rgb(" r "," g "," b ")"))
 
 (def black (rgb 0 0 0))
 (def transparent (rgba 0 0 0 0))
